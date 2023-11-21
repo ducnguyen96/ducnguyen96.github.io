1b169aa3a74f0ed23ceb2d64fd3558f0d3096b9f
ducnguyen96 <levinguyen.dl@gmail.com>
2023-11-21 17:26:16 +0700

    move static files from old projects to this

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,19 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'ducnguyen96',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://ducnguyen96.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ducnguyen96', // Usually your GitHub org/user name.
+	projectName: 'ducnguyen96.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -25,8 +24,8 @@ const config: Config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'vi',
+    locales: ['vi'],
   },
 
   presets: [
@@ -57,23 +56,43 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    algolia: {
+      contextualSearch: true,
+      appId: 'MBM19YJCG1',
+      apiKey: '6537a41be5d500442bc3131e309be3aa',
+      indexName: 'ducnguyen96.github.io',
+    },
     navbar: {
-      title: 'My Site',
+      title: '',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'ducnguyen96',
+        src: 'img/logo.jpeg',
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          type: 'doc',
+          docId: 'intro',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          to: 'https://github.com/ducnguyen96',
           label: 'GitHub',
+          position: 'right',
+        },
+        {
+          to: 'https://www.youtube.com/channel/UCCrvmlNIXLgZOMAMn9VKBeA',
+          label: 'Youtube',
+          position: 'right',
+        },
+        {
+          to: '/projects/cv/',
+          label: 'CV',
+          position: 'right',
+        },
+        {
+          to: '/projects/cv-pdf/',
+          label: 'CV-PDF',
           position: 'right',
         },
       ],
@@ -85,8 +104,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Docs',
+              to: '/docs/introduction',
             },
           ],
         },
@@ -94,34 +113,13 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: 'Youtube',
+              href: 'https://www.youtube.com/channel/UCCrvmlNIXLgZOMAMn9VKBeA',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} ducnguyen96, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
