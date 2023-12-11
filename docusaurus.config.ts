642e3b4a18231e59107345c03c2b8eda9956da62
ducnguyen96 <levinguyen.dl@gmail.com>
2023-12-11 10:29:34 +0700

    blog: new blog - stored-xss

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,54 +1,57 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'ducnguyen96',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'favicon.ico',
+  title: "ducnguyen96",
+  tagline: "Dinosaurs are cool",
+  favicon: "favicon.ico",
 
-  url: 'https://ducnguyen96.github.io',
+  url: "https://ducnguyen96.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-  deploymentBranch: 'gh-pages',
+  baseUrl: "/",
+  deploymentBranch: "gh-pages",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ducnguyen96', // Usually your GitHub org/user name.
-	projectName: 'ducnguyen96.github.io', // Usually your repo name.
+  organizationName: "ducnguyen96", // Usually your GitHub org/user name.
+  projectName: "ducnguyen96.github.io", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
+
+  themes: ["@docusaurus/theme-live-codeblock"],
+
   i18n: {
-    defaultLocale: 'vi',
-    locales: ['vi'],
+    defaultLocale: "vi",
+    locales: ["vi"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -56,35 +59,40 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'docusaurus-social-card.jpg',
+    image: "docusaurus-social-card.jpg",
     algolia: {
       contextualSearch: true,
-      appId: 'MBM19YJCG1',
-      apiKey: '6537a41be5d500442bc3131e309be3aa',
-      indexName: 'ducnguyen96.github.io',
+      appId: "MBM19YJCG1",
+      apiKey: "6537a41be5d500442bc3131e309be3aa",
+      indexName: "ducnguyen96.github.io",
+    },
+    tableOfContents: {
+      minHeadingLevel: 2,
+      maxHeadingLevel: 5,
     },
     navbar: {
-      title: '',
+      title: "",
       logo: {
-        alt: 'ducnguyen96',
-        src: 'logo.jpeg',
+        alt: "ducnguyen96",
+        src: "logo.jpeg",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Docs',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Docs",
+        },
+        { to: "blog", label: "Blog", position: "left" }, // or position: 'right'
+        {
+          to: "https://github.com/ducnguyen96",
+          label: "GitHub",
+          position: "right",
         },
         {
-          to: 'https://github.com/ducnguyen96',
-          label: 'GitHub',
-          position: 'right',
-        },
-        {
-          to: 'https://www.youtube.com/channel/UCCrvmlNIXLgZOMAMn9VKBeA',
-          label: 'Youtube',
-          position: 'right',
+          to: "https://www.youtube.com/channel/UCCrvmlNIXLgZOMAMn9VKBeA",
+          label: "Youtube",
+          position: "right",
         },
         // {
         //   to: '/projects/cv/',
@@ -139,7 +147,7 @@ const config: Config = {
         },
       };
     },
-  ]
+  ],
 };
 
 export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./blog/**/*.{md,mdx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
 
   // daisyUI config (optional - here are the default values)
   daisyui: {
